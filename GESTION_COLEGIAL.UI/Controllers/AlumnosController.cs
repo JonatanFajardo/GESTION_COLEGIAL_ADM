e5a244951113379773e62955291cb643d9f0a135
JonatanFajardo <69583436+JonatanFajardo@users.noreply.github.com>
2021-08-09 23:04:34 -0600

    creacion de archivo de controladores

---
+++ GESTION_COLEGIAL.UI/Controllers/AlumnosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GESTION_COLEGIAL.UI.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GESTION_COLEGIAL.UI/Controllers/SubMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GESTION_COLEGIAL.UI.Controllers
+{
+    public class SubMenuController : Controller
+    {
+        // GET: SubMenu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
