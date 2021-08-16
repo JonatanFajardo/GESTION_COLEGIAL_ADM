b5f90478a3548d20e17a4afbcb9e1f383af7518d
JonatanFajardo <69583436+JonatanFajardo@users.noreply.github.com>
2021-08-15 21:05:13 -0600

    agregando plugin toastr

--- GESTION_COLEGIAL.UI/Controllers/ModalidadesController.cs
+++ GESTION_COLEGIAL.UI/Controllers/ModalidadesController.cs
@@ -21,64 +21,50 @@ namespace GESTION_COLEGIAL.UI.Controllers
             return View();
         }
 
-
-
-
         [HttpPost]
-        // GET: Modalidades/Create
-        public async Task<ActionResult> Create(ModalidadViewModel model)
+        public ActionResult Create()
         {
 
-            //if (!ModelState.IsValid)
-            //{
-
-            //}
-            String url = "https://localhost:44341/api/Modalidades/Create";
-            await SendHttpClient.Post(url, model);
-            return null;
-
-
-
-
-
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = new Uri("http://localhost:64189/api/student");
-
-            //    //HTTP POST
-            //    var response = client.PostAsync("api/AgentCollection", new StringContent(new JavaScriptSerializer().Serialize(user), Encoding.UTF8, "application/json")).Result;
-
-            //    var postJob = client.PostAsJsonAsync< ModalidadViewModel > "model", model);
-            //    postJob.Wait();
-
-            //    var result = postTask.Result;
-            //    if (result.IsSuccessStatusCode)
-            //    {
-            //        return RedirectToAction("Index");
-            //    }
-            //}
-
-            //ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
-
-
-
+            string str = string.Format("Alert('{0}')", "gola");
+            JavaScriptResult result = new JavaScriptResult();
+            result.Script = str;
+            return result;
         }
-
-        //// POST: Modalidades/Create
         //[HttpPost]
-        //public ActionResult Create(FormCollection collection)
+        // GET: Modalidades/Create
+        //public async Task<ActionResult> Create(ModalidadViewModel model)
         //{
-        //    try
+        //    if (!ModelState.IsValid)
         //    {
-        //        // TODO: Add insert logic here
+        //    }
 
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
+        //    string url = "https://localhost:44341/api/Modalidades/Create";
+        //    await SendHttpClient.Post(url, model);
+        //    return View("Index");
+
+        //    //using (var client = new HttpClient())
+        //    //{
+        //    //    client.BaseAddress = new Uri("http://localhost:64189/api/student");
+
+        //    //    //HTTP POST
+        //    //    var response = client.PostAsync("api/AgentCollection", new StringContent(new JavaScriptSerializer().Serialize(user), Encoding.UTF8, "application/json")).Result;
+
+        //    //    var postJob = client.PostAsJsonAsync< ModalidadViewModel > "model", model);
+        //    //    postJob.Wait();
+
+        //    //    var result = postTask.Result;
+        //    //    if (result.IsSuccessStatusCode)
+        //    //    {
+        //    //        return RedirectToAction("Index");
+        //    //    }
+        //    //}
+
+        //    //ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+
+
         //}
+
 
         // GET: Modalidades/Edit/5
         public ActionResult Edit(int id)
