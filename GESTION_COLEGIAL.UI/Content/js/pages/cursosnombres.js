f90a7bb540360a407b6098ed8d732332db3a7193
NayeliGuillen <nayeliguillenramirez@gmail.com>
2021-09-14 22:22:02 -0600

    Correción de vistas

--- GESTION_COLEGIAL.UI/Content/js/pages/cursosnombres.js
+++ GESTION_COLEGIAL.UI/Content/js/pages/cursosnombres.js
@@ -1,1 +1,17 @@
-﻿
+﻿var CursosNombres = (function () {
+
+    var obj = {};
+
+    obj.datatable = function (Direction) {
+        $(function () {
+            var header = new Array();
+            //Nombre | Tamaño/AutoWidth | Visibilidad
+            header = [
+                "Cno_Id",
+                "Cno_Descripcion"
+            ];
+            datatable.init(Direction.listUrl, header);
+        })
+    }
+    return obj;
+}());
--- GESTION_COLEGIAL.UI/Controllers/CursosNombresController.cs
+++ GESTION_COLEGIAL.UI/Controllers/CursosNombresController.cs
@@ -14,5 +14,89 @@ namespace GESTION_COLEGIAL.UI.Controllers
         {
             return View();
         }
+
+        public async Task<ActionResult> List()
+        {
+            string url = "CursosNombres/List";
+            var result = await CatalogsService.List<CursoNombreViewModel>(url);
+            return AjaxResult(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CursoNombreViewModel model)
+        {
+            if (model.Cno_Id == 0)
+            {
+                string url = "CursosNombres/Create";
+                bool result = await CatalogsService.Create(url, model);
+
+                //Validamos error
+                if (result)
+                {
+                    return AjaxResult(false);
+                }
+
+                return AjaxResult(true);
+            }
+            else
+            {
+                string url = "CursosNombres/Edit";
+                bool result = await CatalogsService.Edit(url, model);
+
+                //Validamos error
+                if (result)
+                {
+                    return AjaxResult(false);
+                }
+
+                return AjaxResult(true);
+            }
+
+        }
+
+        public async Task<ActionResult> Find(int id)
+        {
+            string url = "CursosNombres/Find";
+            var result = await CatalogsService.Find<CursoNombreViewModel>(url, id);
+            return AjaxResult(result, true);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Exist(int? Cno_Id, string Cno_Descripcion)
+        {
+            //Validaciones.
+            ValidationModal validationModal = new ValidationModal();
+            validationModal.SendMessage = Cno_Descripcion;
+            validationModal.BlankSpaces();
+            validationModal.SpecialCharacters();
+            if (validationModal.RequestMessage != null)
+            {
+                return Json(validationModal.RequestMessage);
+            }
+
+            //Envío de datos.
+            string url = "CursosNombres/Exist";
+            var result = await CatalogsService.Exist<CursoNombreViewModel>(url, Cno_Descripcion);
+            if (result != null)
+            {
+                int? firstValue = result.First().Cno_Id;
+                return (firstValue == Cno_Id) ? Json(true) : Json(msjExist);
+            }
+            return Json(true);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id)
+        {
+            string url = "CursosNombres/Remove";
+            bool result = await CatalogsService.Delete(url, id);
+
+            //Validamos error
+            if (result)
+            {
+                return AjaxResult(false);
+            }
+            return AjaxResult(true);
+        }
     }
 }
