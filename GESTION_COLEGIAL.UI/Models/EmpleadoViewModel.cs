7a178f259aa94a8f9899f989aa877821b312e6d8
JonatanFajardo <69583436+JonatanFajardo@users.noreply.github.com>
2021-09-22 20:47:17 -0600

    quitando null a string.

--- GESTION_COLEGIAL.UI/Models/EmpleadoViewModel.cs
+++ GESTION_COLEGIAL.UI/Models/EmpleadoViewModel.cs
@@ -34,7 +34,7 @@ namespace GESTION_COLEGIAL.UI.Models
         [Display(Name = "Es activo")]
         [Required(ErrorMessage = "El campo  es requerido")]
         public bool? EsActivo { get; set; }
-        public string? Emp_EsActivo { get; set; }
+        public string Emp_EsActivo { get; set; }
 
     }
 }
