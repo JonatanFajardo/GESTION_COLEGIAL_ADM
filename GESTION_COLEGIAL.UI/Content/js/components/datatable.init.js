66bf10cc205472360b7e2b910c74e39918fab0b4
JonatanFajardo <jonatanfajardo000@gmail.com>
2022-01-27 00:40:42 -0600

    Limpieza en catalogs.

--- GESTION_COLEGIAL.UI/Content/js/components/datatable.init.js
+++ GESTION_COLEGIAL.UI/Content/js/components/datatable.init.js
@@ -11,10 +11,7 @@ var Modals = (function () {
 
     //obj.NewModal
 
-
-        table = $("#datatable");
-
-
+    table = $("#datatable");
 
     function deleteModal(params) {
 
@@ -33,7 +30,6 @@ var Modals = (function () {
 
             case 'delete':
                 $(function () {
-                    console.log('aa' + params);
                     deleteModal(params);
                     $deleteModal.modal("show");
                 });
@@ -354,118 +350,3 @@ var datatable = (function () {
 }());
 
 
-
-//function nuevo() {
-//    //$(formId).trigger("reset");
-//    console.log('add-btn');
-//    $("#item-id").val("0");
-//    //$editModal.modal("show");
-//    //action = "edit"; console.log('add-btn');
-//}
-
-//$("#add-btn").click(function () {
-//    /*$(formId).trigger("reset");*/
-//    console.log('add-btn');
-//    //$("#item-id").val("0");
-//    //$editModal.modal("show");
-//    //action = "edit";
-//});
-
-
-
-//function RedirectEdit(params) {
-//    window.location = '/' + tabla + '/Edit/' + params + '';
-//}
-
-
-
-//appetsConfig.Catalogs = (function () {
-//    var obj = {};
-//    //url de donde sacare la data.
-//    obj.configureTable = function (params) {
-//        $(function () {
-//            var exportOptions = { columns: [0, 1, 2], orthogonal: "export" }, canEdit, canDelete;
-//            var table = $('#datatable').DataTable({
-//                buttons: [
-//                    {
-//                        text: '<i class="mdi mdi- refresh"> Recargar</i>',
-//                        titleAttr: 'Regargar tabla',
-//                        action: function (e, dt, node, config) {
-//                            dt.ajax.reload();
-//                        }
-//                    },
-//                    {
-//                        extend: "collection",
-//                        text: "<i class='mdi mdi-export'></i> Exportar",
-//                        titleAttr: "Exportar esta tabla",
-//                        buttons: [
-//                            {
-//                                extend: "excelHtml5",
-//                                text: "<i class='mdi mdi-file-excel-outline'></i> Excel",
-//                                exportOptions: exportOptions
-//                            },
-//                            {
-//                                extend: "csvHtml5",
-//                                text: "<i class='mdi mdi-file-multiple-outline'></i> CSV",
-//                                exportOptions: exportOptions
-//                            },
-//                            {
-//                                extend: "pdfHtml5",
-//                                text: "<i class='mdi mdi-file-pdf-outline'></i> PDF",
-//                                exportOptions: exportOptions
-//                            }
-//                        ]
-//                    },
-//                    {
-//                        text: '<i class="mdi mdi-plus-thick" id="add-btn" data-toggle="modal" data-target="#edit-modal"> Nuevo</i>',
-//                        attr: {
-//                            title: "Añadir nueva especie",
-//                            id: "add-btn"
-//                        }
-//                    }
-//                ],
-//                ajax: function (data, callback, settings) {
-//                    $.ajax({
-//                        url: params.listUrl,
-//                        type: "GET",
-//                        dataType: "json",
-//                        success: function (response) {
-//                            callback(response);
-//                            table.column(-1).visible(true);
-//                        },
-//                    });
-//                },
-//                columnDefs: [
-//                    {
-//                        targets: 0,
-//                        data: 'espc_Id'
-//                    },
-//                    {
-//                        targets: 1,
-//                        data: 'espc_Descripcion'
-//                    },
-//                    {
-//                        targets: 2,
-//                        className: "text-center",
-//                        width: 80,
-//                        render: function (data, type, row) {
-//                            botones = "";
-//                            if (type == "display") {
-//                                botones += '<button class="btn btn-secondary btn-sm edit-btn ladda-button" data-style="zoom-in" data-id="' + row.espc_Id + '"><span class"ladda-label"><i class="mdi mdi-square-edit-outline"></i></span></button>';
-//                                //botones += '<button class="btn btn-danger btn-sm" onclick="EliminarRole(' + row.comp_Id + ')"><i class="mdi mdi-trash-can"></i></button>';
-//                            }
-//                            return botones;
-//                        }
-//                    }
-//                ]
-//            });
-//        });
-//        $('#datatable').on('init.dt', function () {
-//            $('#add-btn')
-//                .attr('data-toggle', 'modal')
-//                .attr('data-target', '#edit-modal');
-//        });
-//    };
-
-//    return obj;
-//}());
