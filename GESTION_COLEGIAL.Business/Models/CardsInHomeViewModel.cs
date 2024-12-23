b9b89ebd27e4cfdc3b0e1898362c51eaba86b211
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2024-12-23 15:30:24 -0700

    Carga de data en las cards y cambio color en sidebard.

---
+++ GESTION_COLEGIAL.Business/Models/CardsInHomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTION_COLEGIAL.Business.Models
+{
+    public class CardsInHomeViewModel:BaseViewModel
+    {
+        public int Graduados { get; set; }            // Número de graduados
+        public int DiferenciaPromedioAnual { get; set; }  // Diferencia con el promedio anual
+        public int ActualPromedioAnual { get; set; }   // Promedio anual actual
+        public int DiferenciaNuevoIngreso { get; set; }  // Diferencia de nuevo ingreso
+
+    }
+}
