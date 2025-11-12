02ae044d72e5e2e19c406845690096034815fa0b
Jonatan Fajardo <69583436+JonatanFajardo@users.noreply.github.com>
2025-11-12 01:39:41 -0600

    Agregando los viewmodels.

---
+++ GESTION_COLEGIAL.Business/Models/finansas/EstadoPagoDropdownViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace GESTION_COLEGIAL.Business.Models
+{
+    public class EstadoPagoDropdownViewModel
+    {
+        public int EstadoPagoId { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public bool EsEliminado { get; set; }
+        public int UsuarioRegistraId { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public int? UsuarioModificaId { get; set; }
+        public DateTime? FechaModifica { get; set; }
+    }
+}
